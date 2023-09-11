2334ecb84b3ce180e8098e6093e99f3426a2bf98
Prabhu2222 <120732312+Prabhu2222@users.noreply.github.com>
2023-09-11 11:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj={};
+	let arr=[];
+	for(let i=0;i<str.length;i++){
+      let char = str[i];
+		if(obj[char]!=null){
+          obj[char]++;
+		}else{
+			obj[char]=1;
+		}
+	}
+	for(let i=0;i<str.length;i++){
+		let char =str[i];
+		if(obj[char]==1){
+           return char;
+			break;
+		} 
+		
+	}
+	return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
